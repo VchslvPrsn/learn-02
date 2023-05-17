5f5ee557d1f6ec08e56cf5c68f9f9a6db1b165ab
VchslvPrsn <nightmarerl@yandex.ru>
2023-05-17 09:00:24 +0500

    add db

---
+++ todo_list.js
@@ -0,0 +1,19 @@
+async fetchData() {
+    return await fetch('tasks.json')
+        .then(function (response) {
+            return response.json();
+        }).then(function (tasks) {
+            return tasks;
+        })
+}
+async componentDidMount() {
+    let tasks = await this.fetchData()
+    let result = []
+    Object.keys(tasks).map(function (objectKey, index) {
+        var value = tasks[objectKey];
+        result.push(value)
+    });
+
+    this.setState({ items: result })
+
+}
